8adfd77582b9c06827ae4c65537cce26cd020615
Lakshmanp1 <laxman.gdn@gmail.com>
2025-03-22 16:52:57 +0530

    fghj

--- outputs.tf
+++ outputs.tf
@@ -1,24 +1,23 @@
+output "vpc_id" {
+    value = aws_vpc.main.id
+}
+
 # output "azs_info" {
 #     value = data.aws_availability_zones.available
 # }
 
-#  output "subnets_info" {
-#      value = aws_subnet.public
-#  }
-
-output "vpc_id" {
-     value = aws_vpc.main.id
-}
+# output "default_vpc_id" {
+#     value = data.aws_vpc.default.id
+# }
 
 output "public_subnet_ids" {
-     value = aws_subnet.public[*].id 
-
+    value = aws_subnet.public[*].id
 }
 
- output "private_subnet_ids" {
-     value = aws_subnet.private[*].id
- }
+output "private_subnet_ids" {
+    value = aws_subnet.private[*].id
+}
 
- output "database_subnet_ids" {
-    value = aws_subnet.database[*]id
- } 
+output "database_subnet_ids" {
+    value = aws_subnet.database[*].id
+}
